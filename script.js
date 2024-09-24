84a3be564376e05672c8b911be40ddaa386ea42d
Kevin Slawinski <kevin.slawinski@gmail.com>
2024-09-23 20:48:37 -0400

    buttons display value on screen

--- script.js
+++ script.js
@@ -1,6 +1,36 @@
-let first = 0;
-let second = 0;
-let operator = "";
+let first = '0';
+let second = '0';
+let operator = '';
+let input = '';
+
+const output = document.querySelector('.output');
+const buttons = document.querySelectorAll('button');
+
+function setDefaultState() {
+  first = '0';
+  second = '0';
+  operator = '';
+  input = "Hi I'm a calculator";
+  output.innerHTML = input;
+}
+
+buttons.forEach((button) => {
+  button.addEventListener('click', () => {
+    if (button.classList.contains('number')) {
+      input += button.innerHTML;
+      output.innerHTML = input;
+    } else if (button.classList.contains('dot')) {
+      input += button.innerHTML;
+      output.innerHTML = input;
+    } else if (button.classList.contains('operator')) {
+      operator = button.innerHTML;
+    } else if (button.classList.contains('equal')) {
+      operate(first, second, operator);
+    } else if (button.classList.contains('clear')) {
+      setDefaultState();
+    }
+  });
+});
 
 function add(first, second) {
   return first + second;
@@ -20,13 +50,15 @@ function divide(first, second) {
 
 function operate(first, second, operator) {
   switch (operator) {
-    case "+":
+    case '+':
       return add(first, second);
-    case "-":
+    case '-':
       return subtract(first, second);
-    case "*":
+    case '*':
       return multiply(first, second);
-    case "/":
+    case '/':
       return divide(first, second);
   }
 }
+
+setDefaultState();
